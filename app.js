eecb144fc4fd95ec6f77eb3bf98df9d60aa3e451
vineet <vineet.12cs069@abes.ac.in>
2018-02-08 09:59:27 +0530

    practiced es6 syntax and emmiter

--- app.js
+++ app.js
@@ -1,22 +1,81 @@
-const http = require('http');
+// const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 8000;
+// const hostname = '127.0.0.1';
+// const port = 8000;
 
-const server = http.createServer((req, res) => {
-    // console.log(req);
-    res.statusCode = 200;
-    const { method, url} = req;
-    const { headers } = req;
+// const server = http.createServer((req, res) => {
+//     // console.log(req);
+//     res.writeHead(200, { 'Content-Type': 'text/html' });    
+//     var url = req.url;
+    
+//     if (url === '/about') {
+//         res.write('<h1>about us page<h1>'); //write a response
+//         res.end(); //end the response
+//     } else if (url === '/contact') {
+//         res.write('<h1>contact us page<h1>'); //write a response
+//         res.end(); //end the response
+//     } else {
+//         res.write('<h1>Hello World!<h1>'); //write a response
+//         res.end(); //end the response
+//     }
+//     //res.end("Hi I'm listennig you.");
+// });
 
-    console.log( "Request method is" + method);
-    console.log("Request Path is " + url);
+// server.listen(port, hostname, () => {
+//     console.log(`Server running at http://${hostname}:${port}/`);
+// });
 
-    console.log("Full header    ",  headers);
-    res.setHeader('Content-Type', 'text/plain');
-    res.end("Hi I'm listennig you.");
+
+
+const EventEmitter = require('events');
+// var util = require("util");
+// var Person = function(name){
+// this.name= name;
+// }
+class Person extends EventEmitter{
+    constructor(name){
+        super();
+        this.name = name;
+    }
+
+    speak(msg){
+        this.emit('speak', msg);
+    }
+}
+
+
+
+
+// util.inherits(Person, events.EventEmitter);
+
+var person1 = new Person("person1");
+var person2 = new Person("person2");
+var person3 = new Person("person3");
+var person4 = new Person("person4");
+
+var people = [person1, person2, person3, person4]
+
+people.forEach(function (person) {
+    person.on('speak', function (msg) {
+        console.log(person.name + 'said: ' + msg);        
+    }); 
 });
+person1.speak("hey dude" );
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+
+
+
+
+// class MyStream extends EventEmitter {
+
+//     write(data) {
+//         this.emit('data', data);
+//     }
+// }
+
+// const stream = new MyStream();
+
+// stream.on('data', (data) => {
+//     console.log(`Received data: "${data}"`);
+// });
+// stream.write('With ES6');
